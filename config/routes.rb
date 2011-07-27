b45f01d03b3fda073316a843f6ede256856eb772
bl4d3 <mattia.lipreri@gmail.com>
2011-07-27 23:08:24 +0200

    nerdy_book

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 NerdyBook::Application.routes.draw do
   match 'facebooks/index'
   match 'facebooks/logout'
-  match 'facebooks/canvas', :via => :post
+  match 'facebooks/canvas'#, :via => :post
   match 'facebooks/callback'
 
   root :to => "facebooks#index"
